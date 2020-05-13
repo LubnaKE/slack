1d94a7232fd9a970ef21a3e8c9e032ccaecb4ea6
LubnaKE <lubnakhambiye@gmail.com>
2020-05-13 21:47:10 +0800

    add Call.java

---
+++ src/day_2/Call.java
@@ -0,0 +1,9 @@
+package day_2;
+
+public class Call {
+    public static void main(String[] args) {
+        System.out.println("the app can call another participant");
+
+        //this is an intellij file
+    }
+}
