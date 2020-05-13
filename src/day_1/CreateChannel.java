1dc8a3e2990d4056710adc44b309238d09950994
LubnaKE <59313223+LubnaKE@users.noreply.github.com>
2020-05-13 22:59:10 +0800

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,11 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channels");
+        String s= "a";
+       if(s.contains("a")){
+        System.out.println("contains");
+       }else{
+           System.out.println("not");
+}
     }
 }
